f646a3ca2bbdec0c62319c5bf1f4ae59ac3b6d11
Pari <kiaeifar.pardis@gmail.com>
2023-07-16 23:20:00 +0330

    Update SecretFolder.py

--- SecretFolder.py
+++ SecretFolder.py
@@ -1,5 +1,5 @@
 #This program creates a folder in any directory (=C:\ as default) that you can put all your secret files into it
-#Whenever you enter the password (="Password" as default) the status of the folder will change
+#Whenever you enter the password (="PariKia" as default) the status of the folder will change
 #In secret status, it is shown as Recycle Bin and also its content
 #Once you enter the password again, the status will change and you have access to your secret files!
 #MADE BY PARIKIA
